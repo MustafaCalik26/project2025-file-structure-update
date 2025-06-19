ddf44a356303e0c84e4d1a602be87bdf50c86d93
Mustafa Calik <mustafacalik@ogr.eskisehir.edu.tr>
2025-06-19 21:54:53 +0300

    Deploying Project

--- frontend2025/src/api/apiClient.js
+++ frontend2025/src/api/apiClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  // baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
